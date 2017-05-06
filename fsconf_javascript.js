fa2f6fd3f4c0dcd6321428aa53c8905078cb9b6e
张威 <wei.zhangwei@corp.netease.com>
2017-05-07 00:42:12 +0800

    code upload

---
+++ fsconf_javascript.js
@@ -0,0 +1,9 @@
+	FSConf.javascript = {
+                'gallery-alpha': '/media/_/resources/gfx/alpha.png',
+                'gallery-indicator': '/media/_/resources/gfx/indicator.gif',
+                'jwplayer-swf': '/media/_/resources/jwplayer/player.swf',
+                'jwplayer-skin': '/media/_/resources/jwplayer/jw_skin/skin-lightbox.zip',
+                'jwplayer-skin-basic': '/media/_/resources/jwplayer/jw_skin/skin.zip',
+                'jwplayer-skin-audio': '/media/_/resources/jwplayer/jw_skin/skin-audio.zip',
+                'carousel-statements': 'http://staging.wirtgen-cms.oc.kernpunkt.de/staging/_fs5/_marke/com.wirtgen-group.www/en/_/data/statements.php'
+	};
